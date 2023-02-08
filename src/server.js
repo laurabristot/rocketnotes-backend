8c944667262a1f1d539664d8012bc2656cb0a34a
laura bristot <laurabristotbarbi@hotmail.com>
2023-02-08 16:12:22 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -1,18 +1,24 @@
 require('express-async-errors')
-
 const migrationsRun = require('./database/sqlite/migrations')
 const AppError = require('./utils/AppError')
-const express = require('express')
-const routes = require('./routes')
 const uploadConfig = require('./configs/upload')
 
-const app = express()
-app.use(express.json())
 
-app.use(routes)
+const express = require('express')
+const routes = require('./routes')
+const cors = require('cors')
+
+
 migrationsRun()
 
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
 app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
+
+app.use(routes)
 
 app.use((error, req, res, next) => {
   if (error instanceof AppError) {
